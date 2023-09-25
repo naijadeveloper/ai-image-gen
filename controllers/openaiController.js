21b49b34213b01a42fc0fcad23ab13e2b00816a7
Enoch <mmejuenoch@gmail.com>
2023-09-25 21:38:27 +0100

    server setup using express.

---
+++ controllers/openaiController.js
@@ -0,0 +1,13 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const generateImage = async (req, res) => {
+  try {
+    // const response = await
+  } catch (error) {}
+};
+
+exports.generateImage = generateImage;
